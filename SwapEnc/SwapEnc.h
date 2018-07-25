a07a4b1f270ae48ca50bd6029d9f51bab5addd89
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2018-07-25 10:27:25 +0800

    clean up code and improve ui for swapenc

--- SwapEnc/SwapEnc.h
+++ SwapEnc/SwapEnc.h
@@ -7,13 +7,13 @@ namespace enc {
 	class SwapEnc {
 	public:
 		bool on{ true };
-		void prompt();
+		static void name_ver();
+		static void prompt();
 		void __fastcall run_time();
 		void command_line(int argc, char** argv);
 	private:
 		std::ifstream ifs;
 		std::ofstream ofs;
-		void name_ver();
 		void Exit();
 		void __fastcall enc(std::string a);
 	};
