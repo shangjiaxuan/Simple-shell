6ec868c9fd2ea7b4903aec595b6cacb5bae05a02
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2018-07-26 14:44:36 +0800

    Done with support for folders in SwapEnc
    Next step: clean up code

--- SwapEnc/Paths.cpp
+++ SwapEnc/Paths.cpp
@@ -16,10 +16,12 @@ iopath_recursive_iterator iopath_recursive_iterator::end() {
 }
 
 iopath_recursive_iterator& iopath_recursive_iterator::operator++() {
+	//continue iterating over the current directory
 	if (temp != fs::end(temp)) {
 		cur_ipath = temp->path();
 		cur_opath = cur_oroot / cur_ipath.filename();
 		++temp;
+		//start to iterate over the child directory
 		if (fs::is_directory(cur_ipath)) {
 			stack.push(temp);
 			cur_oroot = cur_opath;
@@ -29,11 +31,13 @@ iopath_recursive_iterator& iopath_recursive_iterator::operator++() {
 		return *this;
 	}
 	else {
+		//stack is empty, and the iterator reaches the end, the recursive iteration must also end here
 		if(stack.empty()) {
 			iroot = fs::path();
 			cur_ipath = fs::path();
 			return *this;
 		}
+		//iteration over the current directory is finished, return to continue iterating over the parent directory
 		else {
 			temp = stack.top();
 			stack.pop();
@@ -42,6 +46,7 @@ iopath_recursive_iterator& iopath_recursive_iterator::operator++() {
 			cur_ipath = temp->path();
 			cur_opath = cur_oroot / cur_ipath.filename();
 			++temp;
+			//iterate over child directory
 			if (fs::is_directory(cur_ipath)) {
 				stack.push(temp);
 				cur_oroot = cur_opath;
